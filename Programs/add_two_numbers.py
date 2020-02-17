736ecd61317a0045d22b09e786f6cf155f74dbdd
bats64mgutsi <b.mgutsi@gmail.com>
2020-02-17 11:03:29 +0200

    Added new program 'add_two_numbers.py'

---
+++ Programs/add_two_numbers.py
@@ -0,0 +1,10 @@
+
+# Gets two numbers from the user and prints out the sum
+# Batandwa Mgutsi
+# 17/02/2020
+
+print("Please enter two numbers: ")
+num1 = eval(input())
+num2 = eval(input())
+
+print("The sum is: ", num1+num2)
