05c8120e41cbf9703a587d5cce1002c057902c68
bats64mgutsi <b.mgutsi@gmail.com>
2020-02-17 11:40:47 +0200

    Added new program 'min.py'

---
+++ Programs/min.py
@@ -0,0 +1,12 @@
+
+# Prints the minimum of three numbers
+# Batandwa Mgutsi
+# 17/02/2020
+
+print("Please enter three numbers: ")
+x = eval(input())
+y = eval(input())
+z = eval(input())
+
+min1 = min(x, y)
+print("The minimum is ", min(min1, z))
