df722b8b0ebf53d71659829258449ba6f6bf59b8
bats64mgutsi <b.mgutsi@gmail.com>
2020-02-26 11:23:31 +0200

    Added new program reverse_int.py

---
+++ Programs/reverse_int.py
@@ -0,0 +1,11 @@
+
+# Reverses an integer given by the user
+# Batandwa Mgutsi
+# 26/02/2020
+
+# e.g given 98 the prh=gram should print 89
+# 98 % 10 = 8
+# 98 // 10 = 9
+#
+# continuing:
+# 9 % 10 = 0 terminate
