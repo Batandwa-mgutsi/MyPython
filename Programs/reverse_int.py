041263bf261bd516d706282b9998ba374f752b58
bats64mgutsi <b.mgutsi@gmail.com>
2020-02-28 15:00:04 +0200

    Implemented reverse_int.py

--- Programs/reverse_int.py
+++ Programs/reverse_int.py
@@ -3,9 +3,25 @@
 # Batandwa Mgutsi
 # 26/02/2020
 
-# e.g given 98 the prh=gram should print 89
+# e.g given 98 the program should print 89
 # 98 % 10 = 8
 # 98 // 10 = 9
 #
 # continuing:
-# 9 % 10 = 0 terminate
+# 9 % 10 = 9 terminate
+
+num = eval(input("Please enter a number to print it in reverse: "))
+print("The number in reverse is ", end='')
+
+while num >= 10:
+    # This loop will print all the digits in num from the last
+    # to the 2nd digit. It won't print the first digit.
+
+    # Print the last digit in num
+    print(num % 10, end='')
+
+    # Discard the last digit in num
+    num //= 10
+else:
+    # Print the first digit of num
+    print(num, end='')
