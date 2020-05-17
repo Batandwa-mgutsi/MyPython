629a458957a0704ff71eb5ebab0aef8f030278be
Batandwa Mgutsi <B.mgutsi@gmail.com>
2020-05-17 18:39:38 +0200

    Implemeted num_weeks and weeks

--- Programs/calendar_month.py
+++ Programs/calendar_month.py
@@ -28,7 +28,7 @@ def is_leap(year):
 
 
 def month_num(month_name):
-    """Returns the number of a months, with January as month number 1"""
+    """Returns the number of a month, with January as month number 1"""
     months = [
         'January',
         'February',
@@ -48,6 +48,8 @@ def month_num(month_name):
 
 
 def num_days_in(month_num, year):
+    """Returns the number of days in the given month inthe given year."""
+
     # Febrary has 28 days normally, and 29 in a leap year
     # April, June, September, Nov have 30 days
     # The rest have 31 days
@@ -61,11 +63,73 @@ def num_days_in(month_num, year):
 
 
 def num_weeks(month_num, year):
-    pass
+    """Returns the number of weeks spanned by the given week in the given month."""
+
+    # The first and last weeks are the ones that may not have full days.
+    weeks = 0
+    remainigDays = num_days_in(month_num, year)
+
+    missingDaysInFirstWeek = day_of_week(1, month_num, year) - 1
+    daysInFirstWeek = 7 - missingDaysInFirstWeek
+    weeks += 1
+    remainigDays -= daysInFirstWeek
+
+    fullWeeks = remainigDays // 7
+    weeks += fullWeeks
+    remainigDays -= (fullWeeks*7)
+
+    # Now only the last weeks deyas remain
+    if remainigDays > 0:
+        weeks += 1
+
+    return weeks
+
+
+def add_leading_space(num):
+    if num < 10:
+        return f' {num}'
+    return num
 
 
 def week(week_num, start_day, days_in_month):
-    pass
+    """Returns a string consisting of the day of the month for each day in that
+    week, starting with Monday and ending with Sunday."""
+
+    # Number of days missing should be changed from zero only if week_num is 1
+    numberOfDaysMissing = start_day-1 if week_num == 1 else 0
+
+    # For week 1 the start date is always the 1st of the month
+    # For subsequent weeks, the start date is always on a Monday
+    startDate = 1
+
+    if week_num > 1:
+        # Starting from the 2nd day and iterate until we have encountered
+        # week_num Mondays, the week_numth Monday is the start date for the week.
+
+        # Set initial value to 1 to skip week 1
+        weekCount = 1
+        day = start_day + 1
+        for date in range(2, days_in_month+1):
+            if day > 7:
+                day = 1
+            if day == 1:
+                weekCount += 1
+                if weekCount == week_num:
+                    startDate = date
+                    break
+            day += 1
+
+    # Render the line
+    # The first week should be padded with 2 spaces for every missing day.
+    line = ''.rjust(numberOfDaysMissing*2)
+    for iii in range(0-numberOfDaysMissing, 6-numberOfDaysMissing+1):
+        if(iii < 0) or (startDate+iii > days_in_month):
+            continue
+        else:
+            line += f'{add_leading_space(startDate+iii)} '
+
+    # Ignore the last space
+    return line[0:len(line)-1]
 
 
 def main():
